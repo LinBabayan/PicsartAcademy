5d2eb4d20f8c11e2687e3203eff57c64f408979d
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-03-24 20:48:14 +0400

    Update task4.c

--- hwMar21/task4.c
+++ hwMar21/task4.c
@@ -1,31 +1,4 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <math.h>
-//#include <stdbool.h>
-
-/*bool canDraw(int n){
-//checking if it is possible to draw a triangle using given n by calculating whether it is a sum of numeric sequence(first element = 1, incriment by 1)
-	int i = 1, sum = 0;
-	while(sum < n){
-		sum += i;
-		++i;
-	}
-	
-	if(sum != n)
-		return false;
-	return true;
-	
-//alternative way of checking
-//put the n into the formula of sum of a numeric sequence and check if the count of numbers is a real num
-
-	double D = 1 + 8 * n;
-	int sqrt_D = sqrt(D);
-	if(sqrt_D * sqrt_D == D)
-		return true;
-	return false;
-	
-}*/
-
 
 int main(){
 	
