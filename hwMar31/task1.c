f54909654e95fd78001a5f00fc21f7e0968f3cd7
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-17 13:39:07 +0400

    Update task1.c

--- hwMar31/task1.c
+++ hwMar31/task1.c
@@ -6,10 +6,11 @@ int main(){
     printf("Enter a string: ");
     scanf("%s", str); 
 
-    for (int i = 0; str[i] != '\0'; ++i) 
-        if (str[i] >= 'a' && str[i] <= 'z') 
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
             str[i] &= 0xDF; 
-    
+        }
+    }
     printf("%s\n", str);
     return 0;
 }
