39f623fc282b5bc5b5703b95ff9c7ab527e5051b
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 12:20:25 +0400

    Update task9.c

--- hwApr4/task9.c
+++ hwApr4/task9.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
 void decimalToHex(int n) {
-    printf("Hexadecimal: %X\n", n);
+    char hex[20];
+    int i = 0;
+
+    if (n == 0) {
+        printf("Hexadecimal: 0\n");
+        return;
+    }
+
+    while (n != 0) {
+        int remainder = n % 16;
+
+        // Convert remainder to hex character
+        if (remainder < 10)
+            hex[i] = remainder + '0';
+        else
+            hex[i] = remainder - 10 + 'A';
+
+        n = n / 16;
+        i++;
+    }
+
+    printf("Hexadecimal: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", hex[j]);
+    }
+    printf("\n");
 }
 
 int main() {
