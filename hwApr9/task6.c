9fc6384c3ec4fdede6a0cc447a4d7cfce5b036d7
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 13:30:52 +0400

    Update task6.c

--- hwApr9/task6.c
+++ hwApr9/task6.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 void printArrayReverse(int arr[], int n) {
-	if (n == 0){
-		return;
-	}
-	printf("%d ", arr[n - 1]);
-	printArrayReverse(arr, n - 1);
+    if (n == 0) {
+        return;
+    }
+    printf("%d ", arr[n - 1]);
+    printArrayReverse(arr, n - 1);
+}
+
+void run_reverse_array_program() {
+    int n = 0, arr[100];
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+
+    printArrayReverse(arr, n);
 }
 
 int main() {
-    int n = 0, arr[100];
-    scanf("%d", &n);
-    
-    for (int i = 0; i < n; ++i){
-        scanf("%d", &arr[i]);
-    }
-    
-    printArrayReverse(arr, n);
-    
+    run_reverse_array_program();
     return 0;
 }
-
