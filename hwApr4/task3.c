9db88ebb22d97e9cb5f0810a6021029ff9f82c7f
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 12:37:34 +0400

    task3.c

--- hwApr4/task3.c
+++ hwApr4/task3.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 
-int factorialrec(int n){
-	if(n == 0){
-		return 1;
-	}
-	
-	return n * factorialrec(n - 1);
+void fact() {
+    int n = 0;
+    printf("enter n: ");
+    scanf("%d", &n);
+
+    int factorialrec(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        return n * factorialrec(n - 1);
+    }
+
+    int factorial(int n) {
+        int fact = 1;
+        for(int i = 2; i <= n; ++i) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    printf("fact using recursion: %d\nfact without recursion: %d\n", factorialrec(n), factorial(n));
 }
 
-int factorial(int n){
-
-	int fact = 1;
-	
-	for(int i = 2; i <= n; ++i){
-		fact *= i;
-	}
-	
-	return fact;
-}
-
-int main(){
-	int n = 0;
-	
-	printf("enter n: ");
-	scanf("%d", &n);
-	
-	printf("fact using recursion: %d\nfact without recursion: %d\n", factorialrec(n), factorial(n));
-	
-	return 0;
+int main() {
+    fact();
+    return 0;
 }
