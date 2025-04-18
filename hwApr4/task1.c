cff90488dbe983ee9243248eb2d8ff2261de6299
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 12:34:29 +0400

    Update task1.c

--- hwApr4/task1.c
+++ hwApr4/task1.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-void printTable(int num) {
+void generateMultiplicationTable() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
     for (int i = 1; i <= 10; i++) {
         printf("%d x %d = %d\n", num, i, num * i);
     }
 }
 
 int main() {
-    int num = 0;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    printTable(num);
-
+    generateMultiplicationTable();   
     return 0;
 }
 
