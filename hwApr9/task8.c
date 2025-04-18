c1e1d409c7d09a16d9b23e0896eece5c5f6e0fcd
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 13:32:41 +0400

    Update task8.c

--- hwApr9/task8.c
+++ hwApr9/task8.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
 void printBinary(int n) {
-    if (n == 0){
+    if (n == 0) {
+        printf("0");
         return;
     }
-    
-    printBinary(n / 2);
-    printf("%d", n % 2);
+
+    if (n > 0) {
+        void printBinaryHelper(int n) {
+            if (n == 0) {
+                return;
+            }
+            printBinaryHelper(n / 2);
+            printf("%d", n % 2);
+        }
+        printBinaryHelper(n);
+    }
+
+    printf("\n");
+}
+
+void run_binary_program() {
+    int num = 0;
+    scanf("%d", &num);
+    printBinary(num);
 }
 
 int main() {
-    int num = 0;
-    scanf("%d", &num);
-
-    if (num == 0){
-        printf("0");
-    }
-    else{
-        printBinary(num);
-    }
-    
-    printf("\n");
+    run_binary_program();
     return 0;
 }
-
