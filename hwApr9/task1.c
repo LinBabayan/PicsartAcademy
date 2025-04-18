6c4dfb132476b68bbdd3a1b524ba6dbad5ef4723
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 13:27:06 +0400

    Update task1.c

--- hwApr9/task1.c
+++ hwApr9/task1.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int fibonacci(int n) {
-    if (n < 2){
+    if (n < 2) {
         return n;
-        }
+    }
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main() {
-    int n = 0;
-    
+void run_fibonacci_program() {
+    int n;
     scanf("%d", &n);
     printf("Fibonacci(%d) = %d\n", n, fibonacci(n));
-    
-    return 0;
 }
 
+int main() {
+    run_fibonacci_program();
+    return 0;
+}
