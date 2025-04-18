580bcacdd2077fb2106abffcee883b5ec21e212a
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 13:30:20 +0400

    Update task5.c

--- hwApr9/task5.c
+++ hwApr9/task5.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 void printArray(int *arr, int n) {
-    if (n == 0){
+    if (n == 0) {
         return;
     }
-        
+    
     printArray(arr, n - 1);
     printf("%d ", arr[n - 1]);
 }
 
-int main() {
+void run_array_program() {
     int n = 0, arr[100];
     scanf("%d", &n);
-    for (int i = 0; i < n; ++i){
+    for (int i = 0; i < n; ++i) {
         scanf("%d", &arr[i]);
-	}
-	
+    }
     printArray(arr, n);
-    
-    return 0;
 }
 
+int main() {
+    run_array_program();
+    return 0;
+}
