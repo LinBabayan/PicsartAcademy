27c06bcdbc614da62855a68ef3d03ca3ed1a652a
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 13:04:53 +0400

    Update task8.c

--- hwApr16/task8.c
+++ hwApr16/task8.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+union Data {
+    int i;
+    float f;
+    char c;
+};
+
 void assignInt(union Data *d, int value) {
     d->i = value;
     printf("Assigning int value: %d\n", d->i);
