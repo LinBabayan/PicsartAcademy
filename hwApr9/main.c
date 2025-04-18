eb81fe8981e3a814fcbb94b04f4c694805f904e2
Linolium <128178474+LinBabayan@users.noreply.github.com>
2025-04-18 13:41:44 +0400

    Create main.c

---
+++ hwApr9/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+// Task 1: Fibonacci
+int fibonacci(int n);
+void run_fibonacci_program();
+
+// Task 2: Product of Digits
+int productOfDigits(int n);
+void run_product_program();
+
+// Task 3: Print down to zero
+void printDownToZero(int n);
+void run_print_program();
+
+// Task 4: Print up to N
+void printUpToN(int n);
+void run_print_up_program();
+
+// Task 5: Print Array
+void printArray(int *arr, int n);
+void run_array_program();
+
+// Task 6: Print Array Reverse
+void printArrayReverse(int arr[], int n);
+void run_reverse_array_program();
+
+// Task 7: Sum of digits
+int sumOfDigits(int n);
+void run_sum_program();
+
+// Task 8: Print Binary
+void printBinary(int n);
+void run_binary_program();
+
+// Task 9: Find First Uppercase
+int upper(char a);
+char find_first_uppercase(char *str);
+void run_uppercase_program();
+
+// Task 10: Find Smallest and Largest in Array
+int findSmallest(int arr[], int size);
+int findLargest(int arr[], int size);
+void run_find_smallest_largest_program();
+
+int main() {
+    // Task 1: Fibonacci
+    run_fibonacci_program();
+
+    // Task 2: Product of Digits
+    run_product_program();
+
+    // Task 3: Print down to zero
+    run_print_program();
+
+    // Task 4: Print up to N
+    run_print_up_program();
+
+    // Task 5: Print Array
+    run_array_program();
+
+    // Task 6: Print Array Reverse
+    run_reverse_array_program();
+
+    // Task 7: Sum of digits
+    run_sum_program();
+
+    // Task 8: Print Binary
+    run_binary_program();
+
+    // Task 9: Find First Uppercase
+    run_uppercase_program();
+
+    // Task 10: Find Smallest and Largest in Array
+    run_find_smallest_largest_program();
+
+    return 0;
+}
